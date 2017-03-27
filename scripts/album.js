c816df5cff045b026c9607a4862b9a07e4176499
tadeosss <stadeo17@gmail.com>
2017-03-27 02:30:39 +0000

    Checkpoint 26 commit, not completed.

--- scripts/album.js
+++ scripts/album.js
@@ -34,7 +34,7 @@ var albumMarconi = {
 var createSongRow = function(songNumber, songName, songLength) {
 	var template =
 		'<tr class="album-view-song-item">'
-	+ '  <td class="song-item-number">' + songNumber + '</td>'
+	+ '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
 	+ '  <td class="song-item-title">' + songName + '</td>'
 	+ '  <td class="song-item-duration">' + songLength + '</td>'
 	+ '</tr>'
@@ -44,28 +44,49 @@ var createSongRow = function(songNumber, songName, songLength) {
 };
 
 var setCurrentAlbum = function(album) {
-	// #1
+	// Select all HTML elements to display album page
 	var albumTitle = document.getElementsByClassName('album-view-title')[0];
 	var albumArtist = document.getElementsByClassName('album-view-artist')[0];
 	var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
 	var albumImage = document.getElementsByClassName('album-cover-art')[0];
 	var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 	
-	// #2
+	// firstchild = first child node of element; nodeValue = sets value of node
 	albumTitle.firstChild.nodeValue = album.title;
 	albumArtist.firstChild.nodeValue = album.artist;
 	albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
 	albumImage.setAttribute('src', album.albumArtUrl);
 	
-	// #3
+	// clear album song list HTML to empty string
 	albumSongList.innerHTML = '';
 	
-	// #4
+	// go through all songs from album obj and insert them into HTML using innerHTML
 	for (var i = 0; i < album.songs.length; i++) {
 		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
 	}
 };
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 	
 window.onload = function() {
 	setCurrentAlbum(albumPicasso);
-};
+	
+	songListContainer.addEventListener('mouseover', function(event) {
+		console.log(event.target);
+		// Only target individual song rows during event delegation
+		if (event.target.parentElement.className === 'album-view-song-item') {
+			// Change the content from the number to the play button's HTML
+			event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+		}
+	});
+	
+	for (var i = 0; i < songRows.length; i++) {
+		songRows[i].addEventListener('mouseleave', function(event) {
+			// Selects first child element, which is the song-item-number element
+			this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+		});
+	}
+}
